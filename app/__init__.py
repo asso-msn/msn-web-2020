85207bc8b385a32edad77f8cdcea7eb0b23c7e1d
Tina <me@tina.moe>
2020-11-15 03:07:49 +0100

    Add initial topbar and footer design

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 import importlib
 import os
 from flask import Blueprint, Flask
+from flask_assets import Environment
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
 
+assets = Environment()
 db = SQLAlchemy()
 login = LoginManager()
 migrate = Migrate()
+moment = Moment()
 
 def init_blueprint(module, prefix=False, **kwargs):
     name = module[len(__name__) + 1:]
@@ -21,13 +25,15 @@ def init_blueprint(module, prefix=False, **kwargs):
         **kwargs,
     )
 
-def create_app(config_path='polyrhythm.cfg'):
+def create_app(config_path='xden.cfg'):
     os.environ['CONFIG_FILE'] = os.environ.get('CONFIG_FILE', config_path)
     app = Flask(__name__)
     app.config.from_object(Config)
     app.config.from_envvar('CONFIG_FILE', True)
     login.init_app(app)
+    assets.init_app(app)
     db.init_app(app)
+    moment.init_app(app)
     migrate.init_app(app, db)
 
     for module in ['admin', 'auth', 'main']:
