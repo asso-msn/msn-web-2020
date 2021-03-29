d28dc37f32d0752baf3afc837013d8fe6111244d
Tina <me@tina.moe>
2021-03-29 19:30:36 +0200

    Remove form_route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,14 +2,14 @@ from flask import flash, render_template, redirect, request, url_for
 from flask_login import login_user
 from werkzeug.urls import url_parse
 
-from .. import db
-from ..main.models import User
-from ..view_utils import form_route
+from app import db
+from app.main.models import User
 from . import bp
 from .forms import LoginForm, RegistrationForm
 
-@form_route('/login', bp, LoginForm)
-def login(form):
+@bp.route('/login')
+def login():
+    form = LoginForm()
     def render():
         return render_template('login.html', title='Se connecter', form=form)
     if not form.validate_on_submit():
@@ -26,8 +26,9 @@ def login(form):
         next_page = usual_next
     return redirect(next_page)
 
-@form_route('/register', bp, RegistrationForm)
-def register_post(form):
+@bp.route('/register')
+def register_post():
+    form = RegistrationForm()
     if not form.validate_on_submit():
         return render_template('login.html', title='S\'inscire', form=form)
     user = User(name=form.username.data)
