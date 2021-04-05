b95f94df6caa7dfdac8ef742bd362eb9ee69d546
Tina <me@tina.moe>
2021-04-06 00:02:10 +0200

    Add responsive fixes for nav, home and footer

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -6,22 +6,30 @@ window.onload = (event) => {
   });
 };
 
-
 nav_links = document.querySelectorAll('#navbar section .section-name');
 nav_links.forEach(e => {
   e.onclick = () => {
+    let is_active = e.parentElement.classList.contains('active');
     nav_links.forEach(n =>{
       n.parentElement.classList.remove('active');
     });
-    e.parentElement.classList.add('active');
+    if (!is_active)
+      e.parentElement.classList.add('active');
   };
 });
 
 
-//NAV BURGER
-
-$(document).ready(function(){
-  $('.nav-button').click(function(){
-    $('#navbar').toggleClass('open');
-  });
-});
+// NAV BURGER
+const click_overlay = document.getElementById('click-overlay');
+navbar = document.getElementById('navbar')
+document.getElementById('nav-toggle').onclick = () => {
+    navbar.classList.toggle('open')
+    if (navbar.classList.contains('open'))
+        click_overlay.classList.add('active');
+    else
+        click_overlay.classList.remove('active');
+};
+click_overlay.onclick = () => {
+    navbar.classList.remove('open');
+    click_overlay.classList.remove('active');
+};
