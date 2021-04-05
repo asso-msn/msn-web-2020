4d4efa75eff101c8f9a7ce3cf5056a5a56823c73
Blandine <blandine.talbot14@gmail.com>
2021-04-05 20:23:16 +0200

    Add responsive Navbar

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -6,7 +6,7 @@ window.onload = (event) => {
   });
 };
 
-/*
+
 nav_links = document.querySelectorAll('#navbar section .section-name');
 nav_links.forEach(e => {
   e.onclick = () => {
@@ -16,12 +16,12 @@ nav_links.forEach(e => {
     e.parentElement.classList.add('active');
   };
 });
-*/
 
-//BURGER ANIMATION
+
+//NAV BURGER
 
 $(document).ready(function(){
   $('.nav-button').click(function(){
-    $(this).toggleClass('open');
+    $('#navbar').toggleClass('open');
   });
 });
